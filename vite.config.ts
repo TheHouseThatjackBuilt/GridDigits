986c99caafa34d38d733b5a13b3b0ce7be010456
TheHouseThatjackBuilt <AmericanRolling@gmail.com>
2026-03-10 23:19:15 +0700

    wip vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+const srcPath = decodeURIComponent(new URL("./src", import.meta.url).pathname).replace(
+  /^\/([A-Za-z]:)/,
+  "$1",
+);
+
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/GridDigits/" : "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": srcPath,
+    },
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,
@@ -16,4 +27,8 @@ export default defineConfig({
     port: 4173,
     strictPort: true,
   },
-});
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+}));
